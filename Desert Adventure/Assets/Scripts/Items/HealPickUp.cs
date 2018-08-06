51070ade5a7960ee49f3994414aca9d3611414cb
MDS\sean.hua7281 <>
2018-08-06 16:05:45 +1200

    started making pick up scripts

---
+++ Desert Adventure/Assets/Scripts/Items/HealPickUp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealPickUp : ItemPickUp
+{
+    [Range(1, 6)]
+    public int m_healAmount = 2;
+
+    public override void OnPickUp()
+    {
+        // TODO: Tell player they have healed
+    }
+
+}
---
+++ Desert Adventure/Assets/Scripts/Items/ItemPickUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    SHIP_BASE,
+    SHIP_MAST,
+    SHIP_SAIL,
+    SHIP_TILLER
+}
+
+public abstract class ItemPickUp : MonoBehaviour {
+
+    public abstract void OnPickUp();
+}
