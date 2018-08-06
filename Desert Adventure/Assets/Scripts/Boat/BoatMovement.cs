53622683c46f040ccde7573247383b937e22f848
MDS\sean.hua7281 <>
2018-08-06 15:55:45 +1200

    started some boat stuff. but its way too hard atm

--- Desert Adventure/Assets/Scripts/Boat/BoatMovement.cs
+++ Desert Adventure/Assets/Scripts/Boat/BoatMovement.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class BoatMovement : MonoBehaviour {
 
-    public float m_pushForce = 500;
-    Rigidbody m_rbody;
+    [Header("Speed variables")]
+    public float m_forceApplied = 5000;
+    public float m_maxVelocity = 10;
+    Vector3 m_currentVel, m_refVel;
+
+    Rigidbody m_rbody;  
 
     private void Awake()
     {
         m_rbody = GetComponent<Rigidbody>();
     }
 
-    private void Update()
-    {
-        if (Input.GetButton("Vertical"))
-        {
-            m_rbody.AddForce(transform.up * m_pushForce * Time.deltaTime);
-        }
-
-        float horizontal = -Input.GetAxisRaw("Horizontal");
-        transform.Rotate(new Vector3(0, 0, 45 * horizontal * Time.deltaTime));
-    }
+    
 }
