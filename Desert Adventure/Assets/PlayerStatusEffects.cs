cb175b00780dce1485f0ed605bb33b42427a531b
prunefriend <jwjmair@gmail.com>
2018-08-07 17:22:16 +1200

    Damaged stunned courtines

--- Desert Adventure/Assets/PlayerStatusEffects.cs
+++ Desert Adventure/Assets/PlayerStatusEffects.cs
@@ -6,15 +6,23 @@ public class PlayerStatusEffects : MonoBehaviour {
 
     public float m_flattenedRecoveryTime;
     public float m_flattenedStunTime;
+    public float m_flattenedIncapacitedTime;
     private float m_flattenedTimeStamp;
 
     private Transform m_model;
     private bool m_isFlattened;
 
+    // Player can't move
     public delegate void OnStunned();
     public delegate void OnUnStunned();
     public event OnStunned m_onStunned;
     public event OnUnStunned m_onUnStunned;
+
+    // Player can't use actions
+    public delegate void OnIncapacited();
+    public delegate void OnUnIncapacited();
+    public event OnStunned m_onIncapacited;
+    public event OnUnStunned m_onUnIncapacited;
 
     private void Awake()
     {
@@ -26,14 +34,17 @@ public class PlayerStatusEffects : MonoBehaviour {
         if (!m_isFlattened)
         {
             m_onStunned();
+            m_onIncapacited();
             m_isFlattened = true;
             m_model.localPosition = new Vector3(0.0f, -1.0f, 0.0f);
             m_model.localScale = new Vector3(1.0f, 0.01f, 1.0f);
             StartCoroutine(FlattenedRecovery());
+            StartCoroutine(IncapacitedRecovery(m_flattenedIncapacitedTime));
             StartCoroutine(StunnedRecovery(m_flattenedStunTime));
         };
     }
 
+    
     IEnumerator StunnedRecovery(float stunnedTime)
     {
         yield return new WaitForSeconds(stunnedTime);
@@ -41,7 +52,14 @@ public class PlayerStatusEffects : MonoBehaviour {
         yield return null;
     }
 
-        IEnumerator FlattenedRecovery()
+    IEnumerator IncapacitedRecovery(float incapacitedTime)
+    {
+        yield return new WaitForSeconds(incapacitedTime);
+        m_onUnIncapacited();
+        yield return null;
+    }
+
+    IEnumerator FlattenedRecovery()
     {
         m_flattenedTimeStamp = Time.time;
         while ((m_flattenedTimeStamp + m_flattenedRecoveryTime) >= Time.time)
