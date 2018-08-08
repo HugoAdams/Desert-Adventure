8a713d21fddb548977fc919821e728c652ffd833
MDS\sean.hua7281 <>
2018-08-08 16:35:33 +1200

    boat particles only work when grounded

---
+++ Desert Adventure/Assets/Scripts/Boat/BoatParticles.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatParticles : MonoBehaviour {
+
+    ParticleSystem[] m_particles;
+
+    private void Awake()
+    {
+        m_particles = GetComponentsInChildren<ParticleSystem>();
+        ToggleParticles(false);
+    }
+
+    void ToggleParticles(bool _on)
+    {
+        foreach (ParticleSystem ps in m_particles)
+        {
+            if (_on)
+                ps.Play();
+            else
+                ps.Stop();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // CAN ONLY COLLIDE WITH TERRAIN
+        ToggleParticles(true);
+        Debug.Log("ENTER");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ToggleParticles(false);
+        Debug.Log("EXIT");
+    }
+}
