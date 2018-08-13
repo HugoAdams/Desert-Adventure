5c5183bd9bc4a3963679757da818925e505953c5
MDS\sean.hua7281 <>
2018-08-14 10:26:32 +1200

    updated camera controller. 2 modes depending if on ship or not

--- Desert Adventure/Assets/Scripts/CameraController.cs
+++ Desert Adventure/Assets/Scripts/CameraController.cs
@@ -10,28 +10,36 @@ public enum CameraState
 
 public class CameraController : MonoBehaviour {
 
-    public bool b_auto = true;
+    public bool b_auto = true; // Auto rotate towards player facing dir
     public Transform m_target;
 
-    public float m_radius = 5;
-    float m_angle = 0;
+    float m_radius = 5; // How far away camera is
+    float m_angle = 0; // Y-axis rotation
+    float m_YDiff = 10; // X-axis rotation
 
     public float m_topAngle = 7;
     float m_baseTopAngle;
 
     Vector3 m_targetLastPos;
-    public float m_MaxTurnSpeed = 100;
-
-    public float m_MaxMoveSpeed = 10;
-    float m_YDiff = 10;
+    public float m_yAxisSpeed = 100;
+    public float m_xAxisSpeed = 10;
 
     public float m_timeTillCameraReset = 3;
     float m_cameraResetTimer;
+
+    [Header("Camera mode settings")]
+    public float m_boatStartY = 13;
+    public float m_playerStartY = 8;
+    public float m_boatCameraRadius = 14;
+    public float m_playerCameraRadius = 7;
 
     void Start ()
     {
         m_targetLastPos = transform.position;
         m_baseTopAngle = 5;
+
+        // Assuming starting with player
+        SetToPlayerMode();
     }
 	
 	// Update is called once per frame
@@ -79,9 +87,8 @@ public class CameraController : MonoBehaviour {
     void ResetCameraLogic()
     {
         if (b_auto == false)
-        {
             return;
-        }
+
         float moveAmount = 4;
         float rotateAmount = 20;
 
@@ -100,11 +107,11 @@ public class CameraController : MonoBehaviour {
     {//the horizontal axis amount will multiply m_MaxTurnSpeed
         if (_left)
         {
-            m_angle -= m_MaxTurnSpeed * Time.deltaTime;
+            m_angle -= m_yAxisSpeed * Time.deltaTime;
         }
         else
         {
-            m_angle += m_MaxTurnSpeed * Time.deltaTime;
+            m_angle += m_yAxisSpeed * Time.deltaTime;
         }
         if (m_angle > 360)
         {
@@ -141,7 +148,7 @@ public class CameraController : MonoBehaviour {
     {//the axis amount will be multiplied by m_MaxMoveSpeed
         if(_up)
         {
-            m_YDiff += m_MaxMoveSpeed * Time.deltaTime;
+            m_YDiff += m_xAxisSpeed * Time.deltaTime;
             if(m_YDiff > 15)
             {
                 m_YDiff = 15;
@@ -149,12 +156,40 @@ public class CameraController : MonoBehaviour {
         }
         else
         {
-            m_YDiff -= m_MaxMoveSpeed * Time.deltaTime;
+            m_YDiff -= m_xAxisSpeed * Time.deltaTime;
             if(m_YDiff < 3)
             {
                 m_YDiff = 3;
             }
         }
         Focus();
+    }
+
+    public void SetToBoatMode()
+    {
+        // Start behind player
+        if (m_target)
+        {
+            Vector3 backDir = -m_target.forward;
+            backDir.y = 0;
+            m_angle = Mathf.Atan2(backDir.z, backDir.x) * Mathf.Rad2Deg;
+        }
+
+        m_radius = m_playerCameraRadius;
+        m_YDiff = m_playerStartY;
+    }
+
+    public void SetToPlayerMode()
+    {
+        // Start behind player
+        if (m_target)
+        {
+            Vector3 backDir = -m_target.forward;
+            backDir.y = 0;
+            m_angle = Mathf.Atan2(backDir.z, backDir.x) * Mathf.Rad2Deg;
+        }
+
+        m_radius = m_boatCameraRadius;
+        m_YDiff = m_boatStartY;
     }
 }
