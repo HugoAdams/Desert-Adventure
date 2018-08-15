32f5b1e0acc9ad2574f9a379c3014951c16ce720
MDS\sean.hua7281 <>
2018-08-15 13:50:42 +1200

    main menu done

---
+++ Desert Adventure/Assets/Scripts/UI/Effects/FadeInOut.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FadeInOut : MonoBehaviour {
+
+    TextMeshProUGUI tmp;
+
+    private void Awake()
+    {
+        tmp = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update ()
+    {
+        float alpha = 0.2f * Mathf.Sin(Time.time * 10) + 0.8f;
+        tmp.color = new Color(1, 1, 1, alpha);
+	}
+}
