9f7c7d417343d8526288513a9dfc4fa235e1138b
prunefriend <jwjmair@gmail.com>
2018-08-16 17:21:24 +1200

    Added pot smash particles

--- Desert Adventure/Assets/Scripts/Pots/BreakablePot.cs
+++ Desert Adventure/Assets/Scripts/Pots/BreakablePot.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class BreakablePot : MonoBehaviour {
 
-    public GameObject Heart;
+    public GameObject m_heart;
+    public GameObject m_breakingParticles;
 
     private void OnTriggerEnter(Collider other)
     {
+        Instantiate(m_breakingParticles, transform.position, transform.rotation);
+        if (Random.Range(0,2) == 0)
+        {
+           Instantiate(m_heart, transform.position, Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)));
+        }
         SoundEffectsPlayer.Instance.PlaySound("BreakPot");
         Destroy(gameObject);
-        if(Random.Range(0,2) == 0)
-        {
-           Instantiate(Heart, transform.position, Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)));
-        }
     }
 }
